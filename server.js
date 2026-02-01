27a2d95270683b397949e5c19414214daa60dde0
LamThanh2k04 <ngolam2k04@gmail.com>
2026-02-01 23:17:00 +0700

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello from server Acadex Manager!");
+  res.send("Hello from server Acadex Manager!!!");
 });
 
 app.listen(PORT, () => {
