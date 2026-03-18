5487f6dc613e9e651cd4da1efe88f224c2170c2a
LamThanh2k04 <ngolam2k04@gmail.com>
2026-03-18 10:51:38 +0700

    update

--- src/controllers/student/aiController.js
+++ src/controllers/student/aiController.js
@@ -1,5 +1,6 @@
 import { responseSuccess } from "../../common/helpers/response.helper.js"
-import { aiService } from "../../services/student/AiService.js"
+import { aiService } from "../../services/student/aiService.js"
+
 
 export const aiController = {
     aiMessage: async function (req, res, next) {
